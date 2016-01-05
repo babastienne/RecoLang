17033633a76dc2cdd2a858695822a52a7ca597f8
bpotiron <bpotiron@10.0.2.15>
2016-01-05 15:54:29 +0100

    Initial commit

---
+++ RecoLang/src/langReco/reco/MyLanguageRecognizer1.java
@@ -0,0 +1,56 @@
+package langReco.reco;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import langModel.LanguageModel;
+import langModel.MyLaplaceLanguageModel;
+import langModel.MyNgramCounts;
+import langModel.NgramCounts;
+import langModel.NgramUtil;
+
+public class MyLanguageRecognizer1 extends LanguageRecognizer {
+
+	public MyLanguageRecognizer1() {
+		super();
+		loadNgramCountPath4Lang("lm/fichConfig_bigram-100.txt");
+		recognizeSentenceLanguage("<s> futuro de los sistemas de seguridad social y de pensiones ( breve presentación ) </s>");
+
+	}
+	@Override
+	public String recognizeSentenceLanguage(String sentence) {
+
+		Map<String,String> monMap = new HashMap<String, String>(); // nul
+		int order = 1;
+		Double proba = 0.0;
+		String name = "";
+		String mamerde = "";
+		NgramCounts montruc;
+		LanguageModel frllm;
+		
+		// Parcourir langNgramCountMap
+		for(String lang: langNgramCountMap.keySet()) {
+			
+			for(String idLM: langNgramCountMap.get(lang).keySet()) {
+				
+				montruc = new MyNgramCounts();
+				
+				montruc.scanTextFile(getNgramCountPath(lang, idLM), order);
+				
+				montruc.writeNgramCountFile(mamerde);
+				frllm = new MyLaplaceLanguageModel();
+				montruc.readNgramCountsFile(mamerde);
+				
+				if(frllm.getSentenceProb(sentence) > proba) {
+					proba = frllm.getSentenceProb(sentence);
+					name = lang;
+				}
+			}
+			
+		}
+		
+		return name;
+		
+	}
+
+}
---
+++ RecoLang/src/langReco/reco/appli.java
@@ -0,0 +1,8 @@
+package langReco.reco;
+
+public class appli {
+	
+	public static void main(int[] args) {
+		System.out.println(new MyLanguageRecognizer1());
+	}
+}
