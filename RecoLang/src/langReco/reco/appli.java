3cf432a9dc94f1233ac3a91783ff86119fcae6d4
babastienne <potiron.bastien@gmail.com>
2016-01-06 14:05:48 +0100

    Update appli.java
    Ajout de test pour plusieurs phrases

--- RecoLang/src/langReco/reco/appli.java
+++ RecoLang/src/langReco/reco/appli.java
@@ -2,7 +2,26 @@ package langReco.reco;
 
 public class appli {
 	
-	public static void main(Strings[] args) {
-		System.out.println(new MyLanguageRecognizer1());
+	public static void main(String[] args) {
+		LanguageRecognizer premierEssai = new MyLanguageRecognizer1("lm/fichConfig_bigram-100.txt");
+		String phraseADecoder = "<s> futuro de los sistemas de seguridad social y de pensiones ( breve presentaciÃ³n ) </s>";
+		String langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
+		System.out.println("Phrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		
+		phraseADecoder = "<s> - penso que a comissÃ£o nÃ£o deseja intervir de novo . </s>";
+		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		
+		phraseADecoder = "<s> Ã­rÃ¡sbeli nyilatkozatok ( az eljÃ¡rÃ¡si szabÃ¡lyzat 149. cikke ) </s>";
+		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		
+		phraseADecoder = "<s> declaraÃ§Ãµes escritas inscritas no registo ( artigo 123.Âº do regimento ) : ver acta </s>";
+		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		
+		phraseADecoder = "<s> 3 . 2008. finanÅ¡u gada budÅ¾eta grozÄ«juma nr . 4 projekts ( </s>";
+		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
 	}
 }
