cf03a7a8cde26cac234dcded80f2610062a9b4bc
devel <devel@10.0.2.15>
2016-01-06 16:36:37 +0100

    Ammélioration de MyLanguageRecognizer1
    Reste à amméliorer qualité du code et javadoc + commentaires

--- RecoLang/src/langReco/reco/MyLanguageRecognizer1.java
+++ RecoLang/src/langReco/reco/MyLanguageRecognizer1.java
@@ -1,3 +1,15 @@
+/**
+ * MyLanguageRecognizer1 - first test for hte project
+ * explication supplémentaire si nécessaire
+ * 
+ * @version1.0
+ *
+ * @author Bastien POTIRON
+ * @date 06/01/2016
+ * @notes Still in developpement
+ * 	Amméliorer les commentaire, respecter les normes de qualité de code
+ */
+
 package langReco.reco;
 
 import java.util.ArrayList;
@@ -9,40 +21,42 @@ import langModel.NgramCounts;
 
 public class MyLanguageRecognizer1 extends LanguageRecognizer {
 	
+	/**
+	 * Constructor of the MyLanguageRecognizer1 class.
+	 * 
+	 * @param directoryToConfigurationFile configFile the file path of the configuration file containing the information 
+	 * 	on the language models (language, name and file path).
+	 */
 	public MyLanguageRecognizer1(String directoryToConfigurationFile) {
-		super();
-		super.loadNgramCountPath4Lang(directoryToConfigurationFile);
+		super(); 															// on appel le constructeur de LanguageRecognizer.java
+		super.loadNgramCountPath4Lang(directoryToConfigurationFile);		// on déclare que la variable passée en paramètre contient le chemin du fichier de configuration (liste des langues ...)
 	}
 	
+	/**
+	 * @see langReco.reco.LanguageRecognizer#recognizeSentenceLanguage(java.lang.String)
+	 */
 	@Override
 	public String recognizeSentenceLanguage(String sentence) {
 		
-		LanguageModel frllm;
+		LanguageModel laplaceModel;
 		lang = new ArrayList<String>(getLanguages());
-		Double probab = 0.0;
-		String language="";
-//		for(String l : lang){
-//			System.out.println("l : " + l);
-//			for(String mllm : super.langNgramCountMap.get(l).values()){
-//				System.out.println("mllm : " + mllm);
-//			}
-//		}
+		Double probaLanguePhrase = 0.0;
+		String language = ""; // variable qui sera retournée et contiendra le code pays correspondant à la langue de la phrase transmise en paramètre
 		
-		for(String l : lang){
+		for (String codeLangue : lang) {
+	
+			laplaceModel = new MyLaplaceLanguageModel();
 			
-			for(String mllm : super.langNgramCountMap.get(l).values()){
-				
-				frllm = new MyLaplaceLanguageModel();
-				
-				NgramCounts test = new MyNgramCounts();
-				test.readNgramCountsFile(this.getNgramCountPath(l, l+"_bi"));
-				frllm.setNgramCounts(test);
-				
-				if(frllm.getSentenceProb(sentence) > probab) {
-					probab = frllm.getSentenceProb(sentence);
-					language = l;
-				}
-			}	
+			NgramCounts test = new MyNgramCounts();
+			test.readNgramCountsFile ( this.getNgramCountPath( codeLangue,
+					(String) super.langNgramCountMap.get (codeLangue).keySet ().toArray ()[0]));
+			
+			laplaceModel.setNgramCounts(test);
+			
+			if (laplaceModel.getSentenceProb(sentence) > probaLanguePhrase) { 	//On calcul la probabilité que la phrase soit dans la langue 'codeLangue'
+				probaLanguePhrase = laplaceModel.getSentenceProb(sentence); 	// Si la probabilité est suppérieur à celle calculée avant alors on stocke cette nouvelle probabilité
+				language = codeLangue; 											// On stocke également le code de la langue correspondante (exemple : fr)
+			}
 		}
 		
 		return language;
--- RecoLang/src/langReco/reco/appli.java
+++ RecoLang/src/langReco/reco/appli.java
@@ -1,27 +1,42 @@
+/**
+ * Appli - main class for testing the differents languages recognizers
+ * 
+ * @version1.0
+ *
+ * @author Bastien POTIRON
+ * @date 06/01/2016
+ * @notes This class is in developpement
+ */
+
 package langReco.reco;
 
 public class appli {
 	
+	/**
+	 * Main class
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		LanguageRecognizer premierEssai = new MyLanguageRecognizer1("lm/fichConfig_bigram-100.txt");
 		String phraseADecoder = "<s> futuro de los sistemas de seguridad social y de pensiones ( breve presentaciÃ³n ) </s>";
 		String langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
-		System.out.println("Phrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		System.out.println("Phrase : " + phraseADecoder + "\nLangue trouv�e : " + langue);
 		
 		phraseADecoder = "<s> - penso que a comissÃ£o nÃ£o deseja intervir de novo . </s>";
 		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
-		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouv�e : " + langue);
 		
 		phraseADecoder = "<s> Ã­rÃ¡sbeli nyilatkozatok ( az eljÃ¡rÃ¡si szabÃ¡lyzat 149. cikke ) </s>";
 		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
-		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouv�e : " + langue);
 		
 		phraseADecoder = "<s> declaraÃ§Ãµes escritas inscritas no registo ( artigo 123.Âº do regimento ) : ver acta </s>";
 		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
-		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouv�e : " + langue);
 		
 		phraseADecoder = "<s> 3 . 2008. finanÅ¡u gada budÅ¾eta grozÄ«juma nr . 4 projekts ( </s>";
 		langue = premierEssai.recognizeSentenceLanguage(phraseADecoder);
-		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouvée : " + langue);
+		System.out.println("\nPhrase : " + phraseADecoder + "\nLangue trouv�e : " + langue);
 	}
 }
