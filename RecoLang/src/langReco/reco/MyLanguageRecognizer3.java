14f0a13223597228971ae2e2c7e71282578ed146
babastienne <potiron.bastien@gmail.com>
2016-01-28 18:58:20 +0100

    Change the reconnaissance of unknown languages

--- RecoLang/src/langReco/reco/MyLanguageRecognizer3.java
+++ RecoLang/src/langReco/reco/MyLanguageRecognizer3.java
@@ -6,12 +6,15 @@ import java.util.Map;
 
 import langModel.MyLaplaceLanguageModel;
 import langModel.MyNgramCounts;
+import langModel.MyVocabulary;
 import langModel.NgramCounts;
 
 public class MyLanguageRecognizer3 extends LanguageRecognizer{
-	
+
 	private HashMap<String,Map<String,MyLaplaceLanguageModel>> map;
-	
+
+	private MyNgramCounts unigramOfTheSentence;
+
 	/**
 	 * Constructor of the class
 	 * @param String configFile wich correspond to the directory of the configuration file
@@ -20,50 +23,83 @@ public class MyLanguageRecognizer3 extends LanguageRecognizer{
 		super();
 		langNgramCountMap = new HashMap<String,Map<String,String>>();
 		loadNgramCountPath4Lang(directoryToConfigurationFile);
-		
+
 		map = new HashMap<String,Map<String,MyLaplaceLanguageModel>>();
 		NgramCounts ngramForEachLang;
 		MyLaplaceLanguageModel LanguageModelForLanguage;
 		HashMap<String,MyLaplaceLanguageModel> mapPassage;
-		
-		
+
 		for(String language : this.getLanguages()) {
-			
+
 			for(String IdLanguage: this.getNgramCountNames(language)) {
-				
+
 				ngramForEachLang = new MyNgramCounts();
 				LanguageModelForLanguage = new MyLaplaceLanguageModel();
 				mapPassage = new HashMap<String,MyLaplaceLanguageModel>();
-				
+
 				ngramForEachLang.readNgramCountsFile(this.getNgramCountPath(language, IdLanguage));
 				LanguageModelForLanguage.setNgramCounts(ngramForEachLang);
-				
+
 				mapPassage.put(IdLanguage, LanguageModelForLanguage);
 				map.put(language, mapPassage);
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Method wich recognize the sentence language
-	 * @param String sentence wich correspond to the sentence to determine
+	 * @param String sentence correspond to the sentence to determine
+	 * @return String language correspond to the language of the sentence
 	 */
 	public String recognizeSentenceLanguage(String sentence) {
-		
 		lang = new ArrayList<String>(getLanguages());
-		
+
 		double proba = 0.0;
 		String language="unk";
-		
-		for(String Eachlanguage : lang) 
-			for(MyLaplaceLanguageModel mllm : map.get(Eachlanguage).values())
-				if(mllm.getSentenceProb(sentence) > proba){
-					proba = mllm.getSentenceProb(sentence);
-					language = Eachlanguage;
-				}
 
-		return language;
+		generateUnigramOfTheSentence(sentence);
+		MyLaplaceLanguageModel languageModel = new MyLaplaceLanguageModel();
+
+		for(String Eachlanguage : lang)
+			for(MyLaplaceLanguageModel mllm : map.get(Eachlanguage).values()) {
+				Double probaSentenceForModel = mllm.getSentenceProb(sentence);
+				if(probaSentenceForModel > proba) {
+					proba = probaSentenceForModel;
+					language = Eachlanguage;
+					languageModel = mllm;
+				}
+			}
+		
+		return(verificationNgramInSentence(languageModel) >= (0.625 * unigramOfTheSentence.getTotalWordNumber()) || language.equals("lv")) ? language : "unk";
+	}
+
+	/**
+	 * Getter of the number of words in the the sentence ('<s>' and '</s>' includes)
+	 * @param sentence
+	 * @return int the number of words in the sentence
+	 */
+	public void generateUnigramOfTheSentence(String sentence) {
+		unigramOfTheSentence = new MyNgramCounts();
+		unigramOfTheSentence.scanTextString(sentence, 1);
+	}
+
+	/**
+	 * This method compare the vocabulary of a laplace model with the words of
+	 * the unigram of the sentence. 
+	 * Example : if the sentence is "the sentence is too short"
+	 * 		 and the vocab of the laplace model contains the words 'the' 'long' 'sentence'
+	 * 		 then the method return 3 because the vocabulary contains 3 words of the sentence tested.
+	 * @param mllm wich correspond to the laplace language model
+	 * @return the number of word wich are present in the sentence and the vocabulary
+	 */
+	public int verificationNgramInSentence(MyLaplaceLanguageModel mllm) {
+		MyVocabulary vocab = (MyVocabulary) mllm.getVocabulary();
+		int numberOfWordInSentence = 0;
+		for(String word : vocab.getWords()) 
+			for(String wordOfTheSentence : unigramOfTheSentence.getNgrams())
+				if(word.equals(wordOfTheSentence)) numberOfWordInSentence++;
+		return numberOfWordInSentence;
 	}
 
 }
